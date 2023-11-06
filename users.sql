6433ecd33b2073d1c61a14dc571230d2f4709474
Rasmus3liasson <ras.muse@hotmail.com>
2023-11-06 12:54:26 +0100

    changed the user privileges to the developer account

--- users.sql
+++ users.sql
@@ -1,7 +1,11 @@
 SET GLOBAL validate_password_policy = 'STRONG';
 
-CREATE USER 'utvecklare'@'host' IDENTIFIED BY 'Lösenord_utvecklare123';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER ON bookstore.* TO 'utvecklare'@'host';
+CREATE USER 'developer'@'host' IDENTIFIED BY 'Lösenord_developer123';
+GRANT SELECT, INSERT, UPDATE, DELETE, ALTER ON bookstore.* TO 'developer'@'host';
+
 
 CREATE USER 'webserver'@'host' IDENTIFIED BY 'Lösenord_web123';
 GRANT SELECT, INSERT, UPDATE, DELETE ON bookstore.* TO 'webserver'@'host';
+
+SHOW GRANTS FOR 'developer'@'host';
+
